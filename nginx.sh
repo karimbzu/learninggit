472a47f4a7c3557c5459af262aeb22c83a51aea9
karimbzu <34478122+karimbzu@users.noreply.github.com>
2022-06-25 01:27:28 +0500

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -11,6 +11,6 @@ apt-get -y install nginx
 # Write hostname and IP address to index.html
 mkdir -p /var/www/html
 sed -i -e "s|/usr/share/nginx/html|/var/www/html|g" /etc/nginx/sites-available/default
-echo -e "<html><body><strong>Droplet:</strong> $HOSTNAME<br><strong>IP Address:</strong> $PUBLIC_IPV4</html></body>" \
+echo -e "<html><body><strong>Datibit:</strong> $HOSTNAME<br><strong>IP Address:</strong> $PUBLIC_IPV4</html></body>" \
     > /var/www/html/index.html
 service nginx restart
