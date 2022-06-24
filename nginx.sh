e458f64204592e5fbf82f137f0ad387a04741c76
karimbzu <34478122+karimbzu@users.noreply.github.com>
2022-06-25 01:16:10 +0500

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+export HOSTNAME=$(curl -s http://169.254.169.254/latest/meta-data/hostname)
+export PUBLIC_IPV4=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Install Nginx
+apt-get -y update
+apt-get -y install nginx
+
+# Write hostname and IP address to index.html
+mkdir -p /var/www/html
+sed -i -e "s|/usr/share/nginx/html|/var/www/html|g" /etc/nginx/sites-available/default
+echo -e "<html><body><strong>Droplet:</strong> $HOSTNAME<br><strong>IP Address:</strong> $PUBLIC_IPV4</html></body>" \
+    > /var/www/html/index.html
+service nginx restart
